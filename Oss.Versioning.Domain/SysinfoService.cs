7180d0fe8606de15c3eebcc58c74a68df187beaa
Mike Peretz <mike.peretz@onespan.com>
2020-06-20 16:14:00 -0400

    adding comments

--- Oss.Versioning.Domain/SysinfoService.cs
+++ Oss.Versioning.Domain/SysinfoService.cs
@@ -25,39 +25,51 @@ namespace Oss.Versioning.Domain
 
             dynamic dynamicJson = JValue.Parse(json);
 
+            // sysinfo
             sysInfoData.SysInfoVersion = GetJsonValue("SysInfo Version", 
                 () => { return dynamicJson.sysinfo.version; });
             
+            // Markerting Version 
             sysInfoData.MarketingVersion = GetJsonValue("Markting Version", 
                 () => { return dynamicJson["sender-ui"]["environment"]["RELEASE_VERSION"]; });
             
+            // Sender UI 
             sysInfoData.SenderUIVersion = GetJsonValue("Sender UI Version", 
                 () => { return dynamicJson["sender-ui"]["version"]; });
             
+            // MSC Version 
             sysInfoData.MscVersion = GetJsonValue("MSC Version", 
                 () => { return dynamicJson["msc"].version; });
             
+            // Groovy Version 
             sysInfoData.GroovyVersion = GetJsonValue("Groovy Version", 
                 () => { return dynamicJson["groovy"]["version"]; });
             
+            // Platform Version 
             sysInfoData.PlatformVersion = GetJsonValue("Platform Version", 
                 () => { return dynamicJson["platform"]["version"]; });
 
+            // Signer UI Version 
             sysInfoData.SignerUIVersion = GetJsonValue("Signer UI Version", 
                 () => { return dynamicJson["signer-ui"]["version"]; });
 
+            // Classic UI Version 
             sysInfoData.ClassicUIVersion = GetJsonValue("Classic UI Version",
                 () => { return dynamicJson["classic-ui"]["version"]; });
 
+            // Connector Verison 
             sysInfoData.ConnectorsVersion = GetJsonValue("Connector Version",
                 () => { return dynamicJson["connectors"]["version"]; });
 
+            // Document Engine Version 
             sysInfoData.DocEngineVersion = GetJsonValue("Document Engine Version", 
                 () => { return dynamicJson["doc-engine"]["DE Version"]; });
 
+            // Backend Version 
             sysInfoData.BackendVersion = GetJsonValue("Backend Version", 
                 () => { return dynamicJson["backend"]["version"]; });
 
+            // SSO Version 
             sysInfoData.SsoServiceVersion = GetJsonValue("SSO Service Version", 
                 () => { return dynamicJson["sso-service"]["version"]; });
 
@@ -70,7 +82,7 @@ namespace Oss.Versioning.Domain
             {
                 return getValue();
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 // could not get the field name, report it. 
                 _notify.WriteError($"can't get version for {fieldName}");
